cd74c5a1ad88077187410c76b0d84b67b18c145d
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-19 17:00:40 +0600

    added prepare header function for authorization

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -4,6 +4,15 @@ const apiSlice = createApi({
 	reducerPath: 'api',
 	baseQuery: fetchBaseQuery({
 		baseUrl: 'http://localhost:9000/',
+		prepareHeaders: async (headers, { getState }) => {
+			const token = getState()?.auth?.accessToken;
+
+			if (token) {
+				return headers.set('Authorization', `Bearer ${token}`);
+			}
+
+			return token;
+		},
 	}),
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
