ae09f3abac6250ea2b9f69b7c4c93f2547e9c95e
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-20 08:02:25 +0600

    added user search after writing valid email in modal form

---
+++ src/utils/isValidEmail.js
@@ -0,0 +1,8 @@
+export default function isValidEmail(email = '') {
+	const regex =
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+	const result = email.match(regex);
+
+	return result;
+}
