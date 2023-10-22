64376fa48a78fff1ab23e65e6a7a93b8fa4008c6
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-10-22 21:19:35 +0600

    added logout functionality of access token is expired

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -1,19 +1,33 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { logout } from '../auth/authSlice';
+
+const baseQuery = fetchBaseQuery({
+	baseUrl: 'http://localhost:9000/',
+	prepareHeaders: async (headers, { getState }) => {
+		const token = getState()?.auth?.accessToken;
+
+		if (token) {
+			return headers.set('Authorization', `Bearer ${token}`);
+		}
+
+		return token;
+	},
+});
 
 const apiSlice = createApi({
 	reducerPath: 'api',
-	baseQuery: fetchBaseQuery({
-		baseUrl: 'http://localhost:9000/',
-		prepareHeaders: async (headers, { getState }) => {
-			const token = getState()?.auth?.accessToken;
+	baseQuery: async (args, api, extraOptions) => {
+		let result = await baseQuery(args, api, extraOptions);
 
-			if (token) {
-				return headers.set('Authorization', `Bearer ${token}`);
-			}
+		// remove access token if expired
+		if (result?.error?.status === 401) {
+			api.dispatch(logout());
 
-			return token;
-		},
-	}),
+			localStorage.removeItem('auth');
+		}
+
+		return result;
+	},
 	// eslint-disable-next-line no-unused-vars
 	endpoints: (builder) => ({}),
 });
